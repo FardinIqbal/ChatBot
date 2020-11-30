0b6045e01c99e87df68bbdfcf53370f2832e808c
FardinIqbal <52869603+FardinIqbal@users.noreply.github.com>
2020-11-30 14:45:27 -0500

    Merge with Eddy's repo
    Added all the changes that eddy made

--- src/com/company/ChatBot.java
+++ src/com/company/ChatBot.java
@@ -3,9 +3,15 @@ package com.company;
 public class ChatBot {
     private final String name;
 
+    String[] keyword = {"birth", "weather", "food", "hobby", "age", "gender", "family", "culture", "friend", "movie"};
+    String[] nice = {"love", "hug", "happy", "believe", "awesome", "amazing", "cool"};
+    String[] mean = {"loser", "dumb", "hate", "suck", "die", "kill", "murder", "midget", "nazi", "depressed"};
+
+
     public ChatBot (String name){
         this.name = name;
     }
+
 
     public String getName() {
         return name;
